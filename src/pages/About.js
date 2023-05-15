9537752d964d45fc242061a67006efda235cc185
Mgphone <51868037+Mgphone@users.noreply.github.com>
2023-05-15 18:02:45 +0100

    change about

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,16 @@ const About = () => {
   return (
     <section className='section about-section'>
       <h1 className='section-title'>about page</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae repudiandae architecto qui adipisci in officiis, aperiam sequi atque perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione hic aspernatur error blanditiis?</p>
+      <p>
+The story of the cocktail is a fascinating tale that stretches back through the centuries. The origins of the cocktail are somewhat shrouded in mystery, but most historians believe that it emerged during the early 19th century in America. At this time, drinking alcohol was a common pastime, but the quality of the liquor was often poor and the taste was frequently harsh.<br />
+
+To mask the unpleasant flavors, bartenders began mixing different spirits together with various sweeteners and flavorings. These early cocktails were often quite simple, consisting of just two or three ingredients, but they were a hit with drinkers who appreciated the smoother, more palatable taste.<br />
+
+Over time, the cocktail evolved and became increasingly complex. Bartenders experimented with different ingredients, developing new recipes and techniques to create unique and exciting drinks. Soon, cocktails became a symbol of sophistication and style, with people around the world enjoying these delicious concoctions in trendy bars and clubs.<br />
+
+Today, the cocktail is more popular than ever, with countless variations and styles to choose from. From classic drinks like the Old Fashioned and Martini to more modern creations like the Cosmopolitan and Margarita, there is a cocktail to suit every taste and occasion.<br />
+
+Whether enjoyed as a pre-dinner aperitif, a post-work pick-me-up, or a celebratory drink, the cocktail remains an essential part of modern drinking culture. So next time you raise a glass to toast a special occasion or enjoy a night out with friends, take a moment to appreciate the rich history and legacy of this timeless beverage.</p>
     </section>
   )
 }
